2f5ef759267e7099ad105e0f57e68bf5f062430f
elenamarsili <e.marsili89@gmail.com>
2021-08-24 21:39:26 +0200

    refactor ContactList with hooks

--- web/src/components/contacts/contact-list/ContactList.js
+++ web/src/components/contacts/contact-list/ContactList.js
@@ -1,68 +1,62 @@
-import { Component } from 'react';
+import {useState, useEffect} from 'react'
 import ContactItem from '../contact-item/ContactItem';
 import ContactForm from '../contact-form/ContactForm';
 
 import contactsService from '../../../services/contacts-service';
 
-class ContactList extends Component {
+function ContactList () {
+  const [contacts, setContacts] = useState([])
+  const [isLoading, setLoading] = useState(true)
 
-  state = {
-    contacts: [],
-    isLoading: true
-  }
-
-  fetchContacts() {
+  function fetchContacts(){
     contactsService.list()
-      .then(contacts => this.setState({ contacts, isLoading: false }))
-      .catch(error => {
-        this.setState({ isLoading: false })
-        console.error(error)
-      });
+          .then(contacts => {
+            setContacts(contacts)
+            setLoading(false)
+          })
+          .catch(error => {
+            setLoading(false)
+            console.error(error)
+          })
   }
 
-  componentDidMount() {
-    this.fetchContacts();
+  useEffect(() => {
+    fetchContacts()
+  }, [])
+
+  function handleCreateContact(contact){
+      setContacts([contact, ...contacts])
   }
 
-  handleDeleteContact(id) {
+  function handleDeleteContact(id) {
     contactsService.remove(id)
-      .then(() => this.fetchContacts())
+      .then(() => fetchContacts())
       .catch(error => console.error(error));
   }
 
-  handleCreateContact(contact) {
-    this.setState(({ contacts }) => ({
-      contacts: [contact, ...contacts]
-    }))
-  }
-
-  render() {
-    const { contacts, isLoading } = this.state;
-    return (
-      contacts &&
-        <>
+  return (
+    contacts &&
+      <>
+        <div className="row mb-2">
+          <div className="col">
+            <ContactForm onCreateContact={(contact) => handleCreateContact(contact)}/>
+          </div>
+        </div>
+        {isLoading ? (<i className="fa fa-gear fa-spin"></i>) : (
           <div className="row mb-2">
             <div className="col">
-              <ContactForm onCreateContact={(contact) => this.handleCreateContact(contact)}/>
+              <ul className="list-group">
+                {contacts.map(contact =>
+                  <li key={contact.id} className="list-group-item list-group-item-action">
+                    <ContactItem {...contact} onDeleteContact={(id) => handleDeleteContact(id)} />
+                  </li>
+                )}
+              </ul>
             </div>
           </div>
-          {isLoading ? (<i className="fa fa-gear fa-spin"></i>) : (
-            <div className="row mb-2">
-              <div className="col">
-                <ul className="list-group">
-                  {contacts.map(contact =>
-                    <li key={contact.id} className="list-group-item list-group-item-action">
-                      <ContactItem {...contact} onDeleteContact={(id) => this.handleDeleteContact(id)} />
-                    </li>
-                  )}
-                </ul>
-              </div>
-            </div>
-          )}
-        </>
-    );
-  }
-
+        )}
+      </>
+  );
 }
 
 export default ContactList;
