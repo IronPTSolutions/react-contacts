349074a4dec4e4d0ddf92ef0afac648c663dc8e9
Daniel Abad Noriega <Daniel.Abad.Noriega@gmail.com>
2021-08-26 12:49:34 +0200

    Iteration: contactList refactor Hooks

--- web/src/components/contacts/contact-list/ContactList.js
+++ web/src/components/contacts/contact-list/ContactList.js
@@ -1,68 +1,77 @@
-import { Component } from 'react';
-import ContactItem from '../contact-item/ContactItem';
-import ContactForm from '../contact-form/ContactForm';
+import { Component } from "react";
+import ContactItem from "../contact-item/ContactItem";
+import ContactForm from "../contact-form/ContactForm";
 
-import contactsService from '../../../services/contacts-service';
+import contactsService from "../../../services/contacts-service";
 
 class ContactList extends Component {
+	state = {
+		contacts: [],
+		isLoading: true,
+	};
 
-  state = {
-    contacts: [],
-    isLoading: true
-  }
+	fetchContacts() {
+		contactsService
+			.list()
+			.then((contacts) => this.setState({ contacts, isLoading: false }))
+			.catch((error) => {
+				this.setState({ isLoading: false });
+				console.error(error);
+			});
+	}
 
-  fetchContacts() {
-    contactsService.list()
-      .then(contacts => this.setState({ contacts, isLoading: false }))
-      .catch(error => {
-        this.setState({ isLoading: false })
-        console.error(error)
-      });
-  }
+	componentDidMount() {
+		this.fetchContacts();
+	}
 
-  componentDidMount() {
-    this.fetchContacts();
-  }
+	handleDeleteContact(id) {
+		contactsService
+			.remove(id)
+			.then(() => this.fetchContacts())
+			.catch((error) => console.error(error));
+	}
 
-  handleDeleteContact(id) {
-    contactsService.remove(id)
-      .then(() => this.fetchContacts())
-      .catch(error => console.error(error));
-  }
+	handleCreateContact(contact) {
+		this.setState(({ contacts }) => ({
+			contacts: [contact, ...contacts],
+		}));
+	}
 
-  handleCreateContact(contact) {
-    this.setState(({ contacts }) => ({
-      contacts: [contact, ...contacts]
-    }))
-  }
-
-  render() {
-    const { contacts, isLoading } = this.state;
-    return (
-      contacts &&
-        <>
-          <div className="row mb-2">
-            <div className="col">
-              <ContactForm onCreateContact={(contact) => this.handleCreateContact(contact)}/>
-            </div>
-          </div>
-          {isLoading ? (<i className="fa fa-gear fa-spin"></i>) : (
-            <div className="row mb-2">
-              <div className="col">
-                <ul className="list-group">
-                  {contacts.map(contact =>
-                    <li key={contact.id} className="list-group-item list-group-item-action">
-                      <ContactItem {...contact} onDeleteContact={(id) => this.handleDeleteContact(id)} />
-                    </li>
-                  )}
-                </ul>
-              </div>
-            </div>
-          )}
-        </>
-    );
-  }
-
+	render() {
+		const { contacts, isLoading } = this.state;
+		return (
+			contacts && (
+				<>
+					<div className="row mb-2">
+						<div className="col">
+							<ContactForm
+								onCreateContact={(contact) => this.handleCreateContact(contact)}
+							/>
+						</div>
+					</div>
+					{isLoading ? (<i className="fa fa-gear fa-spin"></i>) : (
+						<div className="row mb-2">
+							<div className="col">
+								<ul className="list-group">
+									{contacts.map((contact) => (
+										<li
+											key={contact.id}
+											className="list-group-item list-group-item-action"
+										>
+											<ContactItem
+												{...contact}
+												onDeleteContact={(id) => this.handleDeleteContact(id)}
+											/>
+										</li>
+									))}
+								</ul>
+							</div>
+						</div>
+					)}
+				</>
+			)
+		);
+	}
 }
 
 export default ContactList;
